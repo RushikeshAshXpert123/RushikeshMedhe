d2d328af5c6c2339bb298cf04f16e137a3dcf8d2
Rushikesh1101 <rushikeshpmedhe@gmail.com>
2025-03-21 12:08:12 +0530

    Deploy2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React, { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { Analytics } from '@vercel/analytics/react';
+
 import './App.css';
 
-const Resume = lazy(() => import('./components/resume.jsx')); // Lazy load Resume component
-
+const Resume = lazy(() => import('./components/resume.jsx')); // Lazy load LoginPage
 function App() {
   return (
-    <Router basename="/RushikeshMedhe/">
+    <Router>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
           <Route path="/" element={<Resume />} />
